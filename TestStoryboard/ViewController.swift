9f604d7634e0f971b27dde0b3031182be77f25f3
sochetra NOV <sochetra12.nov@gmail.com>
2022-09-09 20:26:18 +0700

    Initial Commit

---
+++ TestStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestStoryboard
+//
+//  Created by Sochetra NOV on 9/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
